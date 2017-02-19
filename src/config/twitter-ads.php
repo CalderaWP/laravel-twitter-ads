e54bac1fab1bdbc11801837d7589112db34a9e71
Josh Pollock <Josh@JoshPress.net>
2017-02-19 18:47:51 -0500

    use socialite env details

--- src/config/twitter-ads.php
+++ src/config/twitter-ads.php
@@ -1,7 +1,7 @@
 <?php
 return [
-	'consumer-key' => env('TWITTER_ADS_CONSUMER_KEY'),
-	'consumer-key-secret' => env('TWITTER_ADS_CONSUMER_KEY_SECRET'),
+	'consumer-key' => env('TWITTER_ADS_CONSUMER_KEY', env( 'TWITTER_KEY' ) ),
+	'consumer-key-secret' => env('TWITTER_ADS_CONSUMER_KEY_SECRET', env( 'TWITTER_SECRET' )),
 	'access-key' => env('TWITTER_ADS_ACCESS_KEY'),
 	'access-key-secret' => env('TWITTER_ADS_ACCESS_KEY_SECRET'),
 ];
